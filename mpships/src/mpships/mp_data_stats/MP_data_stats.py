9d7f2f1cfb6e391a5c6c5aea1c7826d9c20b981d
Chiu Peter <chiupeter@Chius-MBP-2.dhcp.lbl.gov>
2025-09-23 12:29:00 -0700

    update latest changes

--- mpships/src/mpships/mp_data_stats/MP_data_stats.py
+++ mpships/src/mpships/mp_data_stats/MP_data_stats.py
@@ -2,7 +2,7 @@ __description__ = "This module provides elemental distribution visualization of 
 __author__ = "Min-Hsueh Chiu"
 
 
-from dash import Dash, html, dcc, callback, Output, Input, MATCH
+from dash import Dash, html, dcc, callback, Output, Input, MATCH, State, ctx
 import plotly.express as px
 import pandas as pd
 import json
@@ -10,9 +10,14 @@ from ptable_plotly import ptable_heatmap_plotly
 from ptable_info import elements_dict, empty_element_count
 import sys
 import uuid
+from collections import defaultdict
 
+from mp_api.client import MPRester
+# import crystal_toolkit.helpers.layouts as ctl
 
-class MPStatsAIO(html.Div):
+API_KEY = "HPnhjtiVgcMGwniGlFFtr877nVA7skf6"
+
+class MPDistAIO(html.Div):
     class ids:
         header = lambda aio:{
             "component": "MPDistAIO",
@@ -20,10 +25,70 @@ class MPStatsAIO(html.Div):
             "subcomponents": "header"
         } 
 
-        endpoint_dropdown = lambda aio:{
+        api_endpoint_dropdown = lambda aio:{
             "component": "MPDistAIO",
             "aio": aio,
-            "subcomponents": "endpoint_dropdown"
+            "subcomponents": "api_endpoint_dropdown"
+        } 
+
+        api_endpoint_histogram = lambda aio:{
+            "component": "MPDistAIO",
+            "aio": aio,
+            "subcomponents": "api_endpoint_histogram"
+        } 
+
+        api_endpoint_div = lambda aio:{
+            "component": "MPDistAIO",
+            "aio": aio,
+            "subcomponents": "api_endpoint_div"
+        } 
+
+        elemental_search_bar = lambda aio:{
+            "component": "MPDistAIO",
+            "aio": aio,
+            "subcomponents": "elemental_search_bar"
+        } 
+
+        ptable_search_bar = lambda aio:{
+            "component": "MPDistAIO",
+            "aio": aio,
+            "subcomponents": "ptable_search_bar"
+        }
+
+        ptable_search_button = lambda aio:{
+            "component": "MPDistAIO",
+            "aio": aio,
+            "subcomponents": "ptable_search_button"
+        }
+
+        ptable_search_reset_button = lambda aio:{
+            "component": "MPDistAIO",
+            "aio": aio,
+            "subcomponents": "ptable_search_reset_button"
+        }
+
+        ptable = lambda aio:{
+            "component": "MPDistAIO",
+            "aio": aio,
+            "subcomponents": "ptable"
+        } 
+
+        ptable_div = lambda aio:{
+            "component": "MPDistAIO",
+            "aio": aio,
+            "subcomponents": "ptable_div"
+        } 
+
+        ptable_title = lambda aio:{
+            "component": "MPDistAIO",
+            "aio": aio,
+            "subcomponents": "ptable_title"
+        } 
+
+        ptable_loader = lambda aio:{
+            "component": "MPDistAIO",
+            "aio": aio,
+            "subcomponents": "ptable_loader"
         } 
 
         whole_graph = lambda aio:{
@@ -32,42 +97,6 @@ class MPStatsAIO(html.Div):
             "subcomponents": "whole_graph"
         } 
 
-
-        histogram = lambda aio:{
-            "component": "MPDistAIO",
-            "aio": aio,
-            "subcomponents": "histogram"
-        } 
-
-        whole_ptable = lambda aio:{
-            "component": "MPDistAIO",
-            "aio": aio,
-            "subcomponents": "whole_ptable"
-        } 
-
-        ptable_endpoint = lambda aio:{
-            "component": "MPDistAIO",
-            "aio": aio,
-            "subcomponents": "ptable_endpoint"
-        } 
-
-        ptable_endpoint_title = lambda aio:{
-            "component": "MPDistAIO",
-            "aio": aio,
-            "subcomponents": "ptable_endpoint_title"
-        }
-
-        ptable_endpoint_element = lambda aio:{
-            "component": "MPDistAIO",
-            "aio": aio,
-            "subcomponents": "ptable_endpoint_element"
-        } 
-
-        ptable_endpoint_element_title = lambda aio:{
-            "component": "MPDistAIO",
-            "aio": aio,
-            "subcomponents": "ptable_endpoint_element_title"
-        } 
 
     ids = ids 
 
@@ -90,91 +119,131 @@ class MPStatsAIO(html.Div):
         self.kwargs = kwargs
 
         # put your layout here 
-        # endpoint_bar
+        # header
+        header = html.H1(
+            children='Materials Project Elemental Data Statistics', 
+            style={'textAlign':'center'},
+            id=self.ids.header(aio_id)
+        )
+
+        # api_endpoint_dropdown
         endpoint_list = [
             'absorption',
             'bonds',
             'dielectric',
             'elasticity',
-            'electronic_structure_bandstructure', 
-            'electronic_structure_dos',
+            # 'electronic_structure_bandstructure', 
+            # 'electronic_structure_dos',
             'electronic_structure',
-            'insertion_electrodes',
+            # 'insertion_electrodes',
             'magnetism',
             'oxidation_states',
             'piezoelectric',
             'summary', 
             'thermo',
         ]
-        endpoint_bar = html.Div([
-                            html.H1(
-                                children='Materials Project Data Statistics', 
-                                style={'textAlign':'center'},
-                                id=self.ids.header(aio_id)
-                            ),
-                            dcc.Dropdown(
-                                options=endpoint_list, 
-                                value='absorption', 
-                                id=self.ids.endpoint_dropdown(aio_id)
-                            )
-                            ]
-                            )
-        # histogram
-        histogram = html.Div(
-            [
-            dcc.Graph(id=self.ids.histogram(aio_id))
-            ], 
+        api_endpoint_dropdown = dcc.Dropdown(
+            options=endpoint_list, 
+            value='absorption', 
+            id=self.ids.api_endpoint_dropdown(aio_id),
             style={
                 'flex': '1',
-                'width': '10%'
+                # 'width': '10%'
             }
         )
 
-        # ptable_endpoint
-        ptable_endpoint = html.Div([
-                    html.H3(
-                        children=f'Elemental distribution', 
-                        style={'textAlign':'center'},
-                        id=self.ids.ptable_endpoint_title(aio_id)
-                    ),
-                    dcc.Graph(
-                            id=self.ids.ptable_endpoint(aio_id)
-                        )
-                ], 
-                style={
-                    'flex': '2'
-                }
-            )
-
-        # ptable_endpoint_element
-        ptable_endpoint_element = html.Div([
-                    html.H3(
-                        children=f'Elemental distribution', 
-                        style={'textAlign':'center'},
-                        id=self.ids.ptable_endpoint_element_title(aio_id)
-                    ),
-                    dcc.Graph(
-                            id=self.ids.ptable_endpoint_element(aio_id)
-                        )
-                ]
-            )
-
-        whole_ptable = html.Div([
-                ptable_endpoint,
-                ptable_endpoint_element
+        # histogram
+        api_endpoint_histogram = html.Div(
+            [
+            dcc.Graph(id=self.ids.api_endpoint_histogram(aio_id))
             ], 
             style={
-                # 'display': 'flex'
+                'flex': '1',
+                # 'width': '10%'
+            }
+        )
+
+        # api_endpoint_div
+        api_endpoint_div = html.Div([
+                api_endpoint_dropdown,
+                api_endpoint_histogram
+            ], 
+            style={
+                # 'flex': '2',
+                'display': 'flex',
+                "border":"2px black solid",
+                'width': '40%',
+                'flexDirection': 'column',
+                'padding': '10px'
             },
-            id=self.ids.whole_ptable(aio_id)
-            )
+            id=self.ids.api_endpoint_div(aio_id)
+        )
+
+        # ptable_title
+        ptable_title = html.H3(
+            children=f'Elemental distribution', 
+            style={'textAlign':'center'},
+            id=self.ids.ptable_title(aio_id)
+        )
+
+        # ptable_search_bar
+        ptable_search_bar = dcc.Input(
+            id=self.ids.ptable_search_bar(aio_id),
+            placeholder="e.g. Al-O",
+            debounce=True
+        )
+
+        # ptable_search_button
+        ptable_search_button = html.Button(
+            'Search',
+            id=self.ids.ptable_search_button(aio_id),
+            n_clicks=0
+        )
+
+        # ptable_search_reset_button
+        ptable_search_reset_button = html.Button(
+            'Reset',
+            id=self.ids.ptable_search_reset_button(aio_id),
+            n_clicks=0
+        )
+
+        # p_table
+        ptable = dcc.Graph(
+            id=self.ids.ptable(aio_id)
+        )
+
+        # loader
+        ptable_loader = dcc.Loading(
+            id=self.ids.ptable_loader(aio_id),
+            type="circle",  # options: "default", "circle", "dot", "cube"
+            children=ptable
+        )
+
+        # ptable_div
+        ptable_div = html.Div([
+                # ptable_title,
+                ptable_search_bar,
+                ptable_search_button,
+                ptable_search_reset_button,
+                ptable_loader
+            ], 
+            style={
+                'flex': '2',
+                'width': '80%',
+                'padding': '10px'
+            },
+            id=self.ids.ptable_div(aio_id)
+        )
+
+    
 
         whole_graph = html.Div([
-                histogram,
-                whole_ptable
+                api_endpoint_div,
+                ptable_div
             ], 
             style={
-                'display': 'flex'
+                'display': 'flex',
+                'width': '100vw'
             },
             id=self.ids.whole_graph(aio_id)
             )
@@ -182,7 +251,6 @@ class MPStatsAIO(html.Div):
 
 
         super().__init__(children=[
-            endpoint_bar,
             whole_graph
 
         ], **kwargs)
@@ -190,15 +258,12 @@ class MPStatsAIO(html.Div):
 
     # put all your callbacks here   
     @callback(
-        Output(ids.histogram(MATCH), 'figure'),
-        Output(ids.ptable_endpoint(MATCH), 'figure'),
-        Output(ids.ptable_endpoint_title(MATCH), 'children'),
-        Input(ids.endpoint_dropdown(MATCH), 'value')
+        Output(ids.api_endpoint_histogram(MATCH), 'figure'),
+        Input(ids.api_endpoint_dropdown(MATCH), 'value')
     )
     def update_fig_enpoint(end_point):
         
-        global api_end_point
-        api_end_point = end_point
+
         
         # histogram
         with open(f"./data/count.json", 'r') as json_file:
@@ -219,69 +284,102 @@ class MPStatsAIO(html.Div):
             yaxis=dict(type='log')
         )
 
-        # chemical elements distribution
-        with open(f"./data/count_ele_{end_point}.json", 'r') as json_file:
-            element_count = json.load(json_file)
-        fig_enpoint = ptable_heatmap_plotly(
-                element_count,
-                # hover_props=["atomic_number", "type"],
-                hover_props=["name"],
-                scaling_factor=0.7,
-            )
-
-        if api_end_point:
-            children_string = f'Elemental distribution at endpoint: {api_end_point}'
-        else:
-            children_string = 'Elemental distribution'  # Default text
-        
-
-        return(fig_hist, fig_enpoint, children_string)
-
+        return(fig_hist)
 
     @callback(
-        Output(ids.ptable_endpoint_element(MATCH), 'figure'),
-        Output(ids.ptable_endpoint_element_title(MATCH), 'children'),
-        Input(ids.endpoint_dropdown(MATCH), 'value'),
-        Input(ids.ptable_endpoint(MATCH), 'clickData'),
+        Output(ids.ptable_search_bar(MATCH), 'value'),
+        Input(ids.ptable_search_reset_button(MATCH), 'n_clicks'),
+        State(ids.ptable_search_bar(MATCH), 'value')
+        # prevent_initial_call=True  # Optional: avoids resetting on page load
     )
-    def update_fig_endpoint_element(end_point, clickData):
-        if clickData:
-            # Access the hover text from clickData
-            try:
-                element_name = clickData['points'][0]['text'].split('<br>')[0]
-            except:
-                element_name = None
-            if element_name:
-                symbol = elements_dict[element_name]
-                
-                # get 
-                with open(f"./data/elemental/count_ele_ele_{api_end_point}.json", 'r') as json_file:
+    def reset_input(n_clicks, current_value):
+        return ''
+
+    @callback(
+        Output(ids.ptable(MATCH), 'figure'),
+        Input(ids.api_endpoint_dropdown(MATCH), 'value'),
+        Input(ids.ptable_search_button(MATCH), 'n_clicks'),
+        Input(ids.ptable_search_reset_button(MATCH), 'n_clicks'),
+        Input(ids.ptable_search_bar(MATCH), 'value'),
+    )
+    def update_ptable(end_point, n_clicks, reset, elements_raw):
+        triggered = ctx.triggered_id
+        if n_clicks == 0 or not elements_raw or triggered == MPDistAIO.ids.ptable_search_reset_button(MATCH):
+            with open(f"./data/count_ele_{end_point}.json", 'r') as json_file:
+                element_count = json.load(json_file)
+            ptable = ptable_heatmap_plotly(
+                    element_count,
+                    # hover_props=["atomic_number", "type"],
+                    hover_props=["name"],
+                    scaling_factor=0.7,
+                )
+            return ptable
+        
+        ptable = None
+        element_list = elements_raw.split('-')
+
+        if len(element_list) == 1:
+            symbol = element_list[0]
+            with open(f"./data/elemental/count_ele_ele_{end_point}.json", 'r') as json_file:
                     element_element_count = json.load(json_file)
                 
-                if symbol in element_element_count:
+            if symbol in element_element_count:
+            
+                # chemical elements distribution on specific element-based
+                ptable = ptable_heatmap_plotly(
+                        element_element_count[symbol],
+                        hover_props=["name"],
+                        scaling_factor=0.7
+                    )
+        else:
+            with MPRester(API_KEY) as mpr:
+                # get all materials id that contatins quired elements
+                summaries = mpr.materials.summary.search(elements=element_list)
+                mat_ids = [doc.material_id for doc in summaries]
+
+            
+                api_class = getattr(mpr.materials, end_point)
+                docs = api_class.search(
+                    # chemsys=elements_raw,
+                    material_ids=mat_ids,
+                    fields=['elements']
+                )
+                """
+                docs = api_class.search(
+                    # chemsys=elements_raw,
+                    elements=element_list,
+                    fields=['elements']
+                )
+                """
+                print('---')
+                print(len(mat_ids))
+                print(len(docs))
+
+
+                element_dict = defaultdict(int)
+
+                for doc in docs:
+                    for element in doc.elements:
+                        element_dict[element.name] += 1
                 
-                    # chemical elements distribution on specific element-based
-                    fig_endpoint_element = ptable_heatmap_plotly(
-                            element_element_count[symbol],
-                            hover_props=["name"],
-                            scaling_factor=0.7
-                        )
+            ptable = ptable_heatmap_plotly(
+                element_dict, 
+                scaling_factor=0.7
+            )
 
 
-                    children_string = f"""
-                            Elemental distribution of ({symbol}-X) in the given endpoint: {api_end_point}
-                        """
+        if not ptable:
+            ptable = ptable_heatmap_plotly(
+                empty_element_count, 
+                scaling_factor=0.7
+            )
+        return(ptable)
+    
 
-                    return(fig_endpoint_element, children_string)
+    
 
-        fig_endpoint_element = ptable_heatmap_plotly(
-            empty_element_count, 
-            scaling_factor=0.7
-        )
-        children_string = f'Elemental distribution at endpoint: {api_end_point}'
-        return(fig_endpoint_element, children_string)
 
 if __name__ == "__main__":
     app = Dash(__name__, suppress_callback_exceptions=True, use_pages=False)
-    app.layout = html.Div(MPStatsAIO(aio="test"))
+    app.layout = html.Div(MPDistAIO(aio="test"))
     app.run_server(debug=True)
